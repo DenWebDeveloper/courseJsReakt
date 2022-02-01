0ca114930deb04ad9b2a21cb456da6b45c90203f
Denis Shabanov <step-klim@mail.ru>
2022-02-02 01:27:31 +0300

    Урок 3 задача 2

--- script.js
+++ script.js
@@ -28,4 +28,49 @@ console.log( screens.toLowerCase().split(', ') );
 
 // Процент, который получит посредник.
 console.log(`Посреднику - ${rollback}% \nЭто целых ${ fullPrice * (rollback/100) } рублей/ долларов/гривен/юани !`);
-console.log("!!!!!");
+let lang = prompt("Выберите язык из представленных", "RU" + "/" +"EN");
+
+// Массив дней недель.
+let daysOfWeek = [
+
+    ['Понедельник', 'Вторник', 'Сроеда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+
+    ['Monday','Tuesday' ,'Wednes­day' , 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+
+    ["ERROR : Вы ввели не правильные значения, будьте внимательны. Набирать все с заглывными буквами!"]
+
+];
+
+// Прверка условий через if.
+
+if(lang === "RU") {
+
+    console.log(daysOfWeek[0]);
+
+}else if(lang === "EN") {
+
+    console.log(daysOfWeek[1]);
+
+}else{
+    console.log(daysOfWeek[2]);
+}
+
+
+// Условие через Switch
+
+switch(lang) {
+    case "RU":
+        console.log(daysOfWeek[0]);
+        break;
+    case "EN":
+        console.log(daysOfWeek[1]);
+        break;
+    default:
+        console.log(daysOfWeek[2]);
+}
+
+
+let namePerson = prompt("Введите имя", "Артем / Александр / любое другое имя");
+
+console.log((namePerson === "Артем") ? "Директор" : (namePerson === "Александр") ? "Преподаватель" : "студент");
+
