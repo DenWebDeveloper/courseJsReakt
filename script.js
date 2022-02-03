a8d6f9200ea60f1e5554e42e25bb41ccf1bd6ae2
Denis Shabanov <step-klim@mail.ru>
2022-02-03 22:01:01 +0300

    Переделан 3 Урок Задание 1

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 
-let  title = "Список дел";
-let  screens = "Простые, Сложные, Интерактивные";
-let  screenPrice = 100;
-let  rollback = 10;
-let  fullPrice = 1000;
-let  adaptive = true;
+// let  title = "Список дел";
+// let  screens = "Простые, Сложные, Интерактивные";
+// let  screenPrice = 100;
+// // let  rollback = 10;
+// let  fullPrice = 1000;
+// let  adaptive = true;
 
 
-console.log(
+// console.log(
 
-    `Типы перемнных: \n ` + 
+//     `Типы перемнных: \n ` + 
 
-    `\t title: ${typeof(title)},
+//     `\t title: ${typeof(title)},
 
-    fullPrice: ${typeof(fullPrice)},
+//     fullPrice: ${typeof(fullPrice)},
 
-    adaptive: ${typeof(adaptive)};`);
+//     adaptive: ${typeof(adaptive)};`);
 
-console.log("Длина строки 'screens' = "+ screens.length  + " " + "символ");
+// console.log("Длина строки 'screens' = "+ screens.length  + " " + "символ");
 
-console.log(`Стоимость верстки экранов: ${screenPrice} рублей/ долларов/гривен/юани`);
+// console.log(`Стоимость верстки экранов: ${screenPrice} рублей/ долларов/гривен/юани`);
 
-console.log(`Стоимость разработки сайта: ${fullPrice} рублей/ долларов/гривен/юани`);
+// console.log(`Стоимость разработки сайта: ${fullPrice} рублей/ долларов/гривен/юани`);
 
-// Перевел строку screens в нижний регист и вывел в массив, затемв console.
-console.log( screens.toLowerCase().split(', ') );
+// // Перевел строку screens в нижний регист и вывел в массив, затемв console.
+// console.log( screens.toLowerCase().split(', ') );
 
-// Процент, который получит посредник.
-console.log(`Посреднику - ${rollback}% \nЭто целых ${ fullPrice * (rollback/100) } рублей/ долларов/гривен/юани !`);
+// // Процент, который получит посредник.
+// console.log(`Посреднику - ${rollback}% \nЭто целых ${ fullPrice * (rollback/100) } рублей/ долларов/гривен/юани !`);
 
 
 // Урок 3 "Динамическая типизация данных в Javascript. Условия, ветвления."
 
 // Выполнение задания
 
-title = prompt("Как называется ваш проект?");
+let title = prompt("Как называется ваш проект?");
 
-screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
 
-screenPrice = +prompt("Сколько будет стоить данная работа?", "пример: " + 12000);
+let screenPrice = +prompt("Сколько будет стоить данная работа?", "пример: " + 12000);
 
-adaptive = confirm("Нужен ли адаптив на сайте?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 
@@ -50,13 +50,13 @@ let service2 = prompt("Какой дополнительный тип услуг
 
 let servicePrice2 = +prompt("Сколько это будет стоить?");
 
-// rollback = +(Math.ceil(Math.random()* 100));
+let rollback = 10;
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
 rollback = +(Math.ceil((rollback / fullPrice)* 100));
 
-
 console.log(rollback);
-
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
 console.log(fullPrice);
 
