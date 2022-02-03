fcee3b0cd3e3e1c1ad32ac7a0ec01adac7cecdfa
Denis Shabanov <step-klim@mail.ru>
2022-02-03 22:31:29 +0300

    Lesson03

--- script.js
+++ script.js
@@ -1,38 +1,7 @@
 
-// let  title = "Список дел";
-// let  screens = "Простые, Сложные, Интерактивные";
-// let  screenPrice = 100;
-// // let  rollback = 10;
-// let  fullPrice = 1000;
-// let  adaptive = true;
-
-
-// console.log(
-
-//     `Типы перемнных: \n ` + 
-
-//     `\t title: ${typeof(title)},
-
-//     fullPrice: ${typeof(fullPrice)},
-
-//     adaptive: ${typeof(adaptive)};`);
-
-// console.log("Длина строки 'screens' = "+ screens.length  + " " + "символ");
-
-// console.log(`Стоимость верстки экранов: ${screenPrice} рублей/ долларов/гривен/юани`);
-
-// console.log(`Стоимость разработки сайта: ${fullPrice} рублей/ долларов/гривен/юани`);
-
-// // Перевел строку screens в нижний регист и вывел в массив, затемв console.
-// console.log( screens.toLowerCase().split(', ') );
-
-// // Процент, который получит посредник.
-// console.log(`Посреднику - ${rollback}% \nЭто целых ${ fullPrice * (rollback/100) } рублей/ долларов/гривен/юани !`);
-
-
 // Урок 3 "Динамическая типизация данных в Javascript. Условия, ветвления."
 
-// Выполнение задания
+// Выполнение задания 1
 
 let title = prompt("Как называется ваш проект?");
 
