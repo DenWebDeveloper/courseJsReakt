48ca4f2cfd59e2ca878ce85b13da14fe78c977e1
Denis Shabanov <step-klim@mail.ru>
2022-02-06 19:37:53 +0300

    Заершене Урока 4 Задачи 2

--- script.js
+++ script.js
@@ -1,74 +1,30 @@
 "use strict";
 
-let string = prompt("sfsf");
 
-// console.log(typeof string);
+let change = function(arg){
 
-
-
-
-const foo = function(arg) {
-    let str = string.trim();
+    let res = "";
     
-    // console.log(str, typeof str);
 
-    
-    if (typeof arg ==="string") {
-        
-        const maxLength = 5;
-        
+    if (typeof arg === "string") {
+
+        const maxLength = 30;
+        const str = arg.trim();
 
         if (str.length > maxLength) {
-            
-            str = str.substr(0,maxLength) + '...';
-            console.log("Вы ввели очень длинную строку...");
-
-        } else {
-            str = str;
-
-            console.log("Вы ввели все корректно...");
-            
+            res = str.substr(0,maxLength) + '...';
+            console.log("Строка более 30 символов - ну очень при очень длинная строчка получилась!");
+        } else if(str.length <= maxLength){
+            res = str;
+            console.log("Ввели все корректно");
         }
-
-    }else{
-
-        let a = str;
-        console.log(a);
-
-        const arrayNum = string;  //"1","2","3","4","5","6","7","8","9","0"
-        console.log(arrayNum);
-
-        for(let i = 0; i < arrayNum.length; i++){
-
-
-            if(string === arrayNum[i]){
-                alert("Вы ввели числа....");
-            }
-        }
-      
+        
+    }else if(typeof arg === "number"){
+        console.log("Вы ввели цыфры");
     }
-    return string;
+  
+    return res;
 };
-console.log(string);
 
-
-
-let a = string;
-console.log(a);
-
-const arrayNum = ["1","2","3","4","5","6","7","8","9","0"];
-
-for(let i = 0; i < arrayNum.length; i++){
-
-
-    if(a === arrayNum[i]){
-        alert("Вы ввели числа....");
-    }
-}
-
-foo(string);
-// console.log(r);
-
-// console.log('foo(): ', foo(5));
-// console.log('foo(): ', foo('    Строка менее 30 символов     '));
-// console.log('foo(): ', foo('    Строка более 30 символов - ну очень при очень длинная строчка получилась!'));
+change("Здесь можно что-то ввести..."); //Здесь в качестве аргумента нужно что-то указать, 
+//чтобы увидеть результат. Пример: change("Какой-то текст || набрать цифры")
