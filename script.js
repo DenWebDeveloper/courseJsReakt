fe1f2d1690244cccf80ff1bebde5c4212d2c7249
Denis Shabanov <step-klim@mail.ru>
2022-02-06 13:13:15 +0300

    Второе изменение

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 // Объявление переменных
 
-let  title = "Список дел";
+let  title = "     CПИСОК дел";
 let  screens = "Простые, Сложные, Интерактивные";
 let  screenPrice = 100;
 let  rollback = 10;
@@ -11,60 +11,103 @@ let  fullPrice = 1000;
 let  adaptive = true;
 
 let servicePrice1 = +prompt("Сколько это будет стоить?", "2000");
-
 let servicePrice2 = +prompt("Сколько это будет стоить?", "3000");
-
 
 
 
 // Объявление функций
 
-const showTypeOf = function(type) {
-    console.log(type, typeof type);
+
+
+// Объявить функцию getAllServicePrices.
+//  Функция возвращает сумму всех дополнительных услуг. 
+//  Результат сохраняем в переменную allServicePrices. 
+//  Тип - function expression
+
+
+//   1 Возвращает сумму всех доп. услуг (Тип - function expression)
+
+
+let allServicePrice = function getAllServicePrices (serPrice1, serPrice2) {
+
+    return serPrice1 + serPrice2;
+
 };
 
-let allServicePrice = function(serPrice1, serPrice2) {
-    return serPrice1 + serPrice2;
-};
+
+/*  Объявить функцию getFullPrice. 
+    Функция возвращает сумму стоимости верстки и стоимости дополнительных 
+    услуг (screenPrice + allServicePrices). Результат сохраняем в переменную
+    fullPrice. Тип - function declaration 
+*/
+
+
+
+// Возвращает сумму стоимости верстки и стоимости дополнительных услуг (Тип - function declaration)
 
 function getFullPrice(a, b) {
+
     return a + b;
+
 }
 
+/*
+    Объявить функцию getTitle. 
+    Функция возвращает title меняя его таким образом: первый символ с большой буквы, остальные с маленькой". 
+    Учесть вариант что строка может начинаться с пустых символов.
+    " КаЛьКулятор Верстки"
+*/ 
+
+let getTitle = function(str){
+
+    str = str.trim();
+
+    return str.charAt(0).toUpperCase(0) + str.slice(1).toLowerCase();
+};
+
+
+/*  Объявить функцию getServicePercentPrices. 
+    Функция возвращает итоговую стоимость за вычетом процента отката. 
+    Результат сохраняем в переменную servicePercentPrice (итоговая стоимость минус сумма отката)
+*/
+
+
+servicePercentPrice
+
+
+//  5 Выводит в консоль значание тииа переменных
+
+let  showTypeOf = function(item) {
+
+    console.log(item, typeof item);
+    
+};
 
 
 // Запуск функий
 
-fullPrice = getFullPrice();
-
 allServicePrice(servicePrice1, servicePrice2);
 
-getFullPrice(screenPrice, allServicePrice);
+fullPrice = getFullPrice(screenPrice, allServicePrice);
+
+getTitle(title);
+
+showTypeOf(getTitle(title));
+
+showTypeOf(fullPrice);
+
+showTypeOf(adaptive);
 
 
 
+// Консоль Логи
+
+console.log(getTitle(title));
 
 
 
-console.log(
+console.log(screens);
+// console.log(getRollbackMessage(fullPrice));
+// console.log(servicePercentPrice);
 
-    `Типы перемнных: \n ` + 
 
-    `\t title: ${typeof(title)},
-
-    fullPrice: ${typeof(fullPrice)},
-
-    adaptive: ${typeof(adaptive)};`
-);
-
-console.log("Длина строки 'screens' = "+ screens.length  + " " + "символ");
-
-console.log(`Стоимость верстки экранов: ${screenPrice} рублей/ долларов/гривен/юани`);
-
-console.log(`Стоимость разработки сайта: ${fullPrice} рублей/ долларов/гривен/юани`);
-
-// Перевел строку screens в нижний регист и вывел в массив, затемв console.
-console.log( screens.toLowerCase().split(', ') );
-
-// Процент, который получит посредник.
-console.log(`Посреднику - ${rollback}% \nЭто целых ${ fullPrice * (rollback/100) } рублей/ долларов/гривен/юани !`);
