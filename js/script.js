bfe3e9d34eca0836596cf297c736b8c4577ba2e2
Denis Shabanov <step-klim@mail.ru>
2022-02-13 15:08:00 +0300

    Урок 10 первое задание

--- js/script.js
+++ js/script.js
@@ -1,135 +1,63 @@
 
-// Задание 1 Урок 9
+    // Поучаю DOM элементы.
+    const body = document.querySelector('body');
+    const books = document.querySelectorAll('.book');
+    const linkBook3 = books[4].querySelector('a');
+    const listItemsBook2 = books[0].querySelectorAll('li'); // Получил главы со второй книги
+    const listItemsBook5 = books[5].querySelectorAll('li'); // Получил главы с пятой книги
 
-// Объявление переменных
 
-const appData = {
+    const advertising = document.querySelector('.adv'); // Реклама.
+
+    // Отработка функций
+
+    // Изменение растановки элементов в правильном порядке
+
+    books[1].after(books[0]);
+    books[0].after(books[4]);
+    books[4].after(books[3]);
+    books[3].after(books[5]);
+
+    // Изменение background у BODY
+    body.style.backgroundImage = 'url("./image/adv.jpg")';
+
+    // Изменение текста в третей книге
+    linkBook3.textContent = "Книга 3. this и Прототипы Объектов ";
+
+
+    // Удаление рекламы
+
+    advertising.remove();
+
+    // Востановить порядок глав во второй книге.
     
-    title: "",
-    screens: "",
-    screenPrice: 0,
-    rollback: 10,
-    adaptive: true,
-    allServicePrice: 0,
-    fullPrice: 0,
-    servicePercentPrice: 0,
-    services: {},
- 
-    start: () => {
-        // вопросы заказчику
-        appData.asking();
-        appData.getAllServicePrice();
-        appData.getFullPrice();
-        appData.getServicePercentPrices();
-        appData.getTitle();
+    listItemsBook2[1].after(listItemsBook2[3]);
+    listItemsBook2[9].after(listItemsBook2[2]);
+    listItemsBook2[3].after(listItemsBook2[6]);
+    listItemsBook2[4].after(listItemsBook2[8]);
+    listItemsBook2[8].after(listItemsBook2[4]);
+    listItemsBook2[6].after(listItemsBook2[8]);
+
+
+    // Востановить порядок глав в пятой книге.
+
+    listItemsBook5[10].after(listItemsBook5[4]);
+
+
+
+
+    //в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
+
+
+    // Вывод консоли
+    console.log(body);
+    console.log(books,"Список Книг");
+    console.log(linkBook3.textContent);
+    console.log(listItemsBook2 ,"Список глав в книге 2");
+    console.log(listItemsBook5 ,"Список глав в книге 5");
+
+
     
-        // сообщения в консоль
-        appData.loger();
-    },
 
-    isNum : function (num) {
 
-        return !isNaN(parseFloat(num) && isFinite(num));
-    },
-
-    loger: function (){
-        for (let key in appData) {
-          let type = typeof (appData[key]);
     
-          console.log(key, (type === 'function' ? '' : appData[key]), '(' + type + ')');
-
-        }
-    
-    },
-
-    asking : function(){
-
-
-        do {
-            appData.title = prompt("Как называется ваш проект", "Пример: Калькулятор верстки");
-        } while(appData.isNum(appData.title) || appData.title === "");
-
-        do {
-            appData.screens = prompt("Простые, Сложные, Интерактивные", "Простые, Сложные и т.д.");
-        } while(appData.isNum(appData.screens) || appData.screens === "");
-
-        do {
-            appData.screenPrice = +prompt("Склько будет стоить данная услуга");
-        } while(!appData.isNum(appData.screenPrice) || appData.screenPrice === 0);
-
-
-        for(let i = 0; i < 2 ; i++){
-
-            let name;
-            let price = 0;
-
-            do {
-                name = prompt("Какой дополнительный тип услуги нужен?");
-            } while (appData.isNum(name) || name === "");
-
-
-            do {
-
-                price = +prompt("Сколько это будет стоить?");
-            } while (!appData.isNum(price) || price === 0);
-
-            appData.services[name] = +price;
-
-        }
-
-        appData.adaptive = confirm("Нужен ли адаптив");
-
-    },
-
-    getAllServicePrice : function() {
-
-        for(let key in appData.services){
-            appData.allServicePrice += appData.services[key];
-        }
-    
-    },
-
-    getFullPrice: function() {
-
-        appData.fullPrice =  +appData.screenPrice + appData.allServicePrice;
-    
-    },
-
-    getTitle : function(){
-
-        appData.title =  appData.title.trim()[0].toUpperCase() + appData.title.trim(1).substr(1).toLowerCase();
-    },
-
-    getServicePercentPrices: function () {
-
-
-        appData.servicePercentPrice =  appData.fullPrice - (appData.fullPrice * (appData.rollback / 100)); 
-        
-    
-    },
-
-    getRollbackMessage: function(fullPrice) {
-    
-        if(fullPrice >= 30000) {
-    
-            console.log("Даем скидку в 10%");
-    
-        }else if(fullPrice >= 15000 && fullPrice < 30000) {
-    
-            console.log("Даем скидку в 5%");
-    
-        }else if(fullPrice < 15000 && fullPrice >= 0 ) {
-    
-            console.log("Скидка не предусмотрена");
-    
-        }else {
-    
-            console.log("Что то пошло не так");
-    
-        }
-    },
-
-
-};
-
-appData.start();
