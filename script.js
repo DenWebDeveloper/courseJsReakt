6871700a0c74f1a3fc563588a4c613e97e0174bf
Denis Shabanov <step-klim@mail.ru>
2022-02-26 16:11:52 +0300

    Урок 15 готов

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 const DomElement = function (selector, height,width, bg, fontSize) {
 
-    this.selector = selector,
-    this. height = height,
-    this.width = width,
-    this.bg = bg,
-    this.fontSize = fontSize
+    this.selector = selector;
+    this. height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
 
 };
 
